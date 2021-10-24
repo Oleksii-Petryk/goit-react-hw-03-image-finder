28c87ca55d5bc2b1f772f7fd0f9101a4355cf9bd
Oleksii <Alex13867777@gmail.com>
2021-10-25 01:09:38 +0300

    [Update]_created components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React, { Component } from 'react';
 import './App.css';
+import Loader from 'react-loader-spinner';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Loader
+          type="Bars"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={10000}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
